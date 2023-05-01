ec49eb33acfb407ffc3d7c00cd39367c6d004525
Ladan Nazari <ladannazary@gmail.com>
2023-05-01 11:30:43 -0700

    frontend started

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-post-create',
+    templateUrl: './post-create.component.html',
+    styleUrls: [] 
+})
+
+export class PostCreateComponent {
+
+    newPost= '';
+
+  onAddPost() {
+    this.newPost = 'The user\'s post'
+  }
+}
+
